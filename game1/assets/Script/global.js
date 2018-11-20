9cbbf5fdb9be5ef5f26da3852da3d4c2dc929e1b
linhaifeng <linhaifeng@yy.com>
2018-11-20 19:03:39 +0800

    chore: 更新笔的射击动画

---
+++ game1/assets/Script/global.js
@@ -0,0 +1,34 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        const pens =  cc.find('Canvas/pens');
+        const pensCom = pens.getComponent('pen');
+        // console.log();
+        this.node.on('mousedown', ()=>{
+            console.log(1);
+            pensCom.move()
+        })
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
---
+++ game1/assets/Script/pen.js
@@ -0,0 +1,42 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        anim: null,
+    },
+
+    start () {
+        const pens = cc.find('Canvas/pens'); 
+        this.anim = pens.getComponent(cc.Animation);// 获取节点中的动画组件
+        const animState =  this.anim.play('pen'); // 播放动画
+        // 播放速度
+        animState.speed = 0.5;
+        // 设置循环模式为 Loop
+        animState.wrapMode = cc.WrapMode.Loop;
+        // anim.runAction(move);
+        // this.node.on('mousedown',  ( event ) => {
+        //     this.anim.pause('pen');
+        //     const action = cc.moveTo(1,0, 100);
+        //     this.node.runAction(action);
+        // });
+       
+    },
+    // 发射
+    move() {
+        this.anim.pause('pen');
+        const action = cc.moveTo(.5, 0, 400);
+        this.node.runAction(action);
+    }
+
+    // update (dt) {},
+});
